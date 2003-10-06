aead4349fab4070e893ff6f73cd3c68445440cb7
John van Groningen <johnvg@cs.ru.nl>
2003-10-06 12:36:25 +0000

    makefiles for windows

---
+++ remove_tmp_files.bat
@@ -0,0 +1,17 @@
+del istartup.a
+del istartup.b
+del istartup.cpp
+del wcon.a
+del wcon.s
+del iprofile.a
+del iprofile.b
+del ifileIO3.a
+del ifileIO3_.s
+del w2fileIO3.a
+del w2fileIO3.s
+del _startup1.s
+del _startup2.s
+del _startup1.go
+del itrace.a
+del itrace.b
+
