598972ca329b05a12af25476fc5b68a718cf28a4
John van Groningen <johnvg@cs.ru.nl>
2003-10-20 10:26:25 +0000

    remove wfileIO3.[as] files instead of w2fileIO3.[as] files, remove wcon_imports temp files

--- remove_tmp_files.bat
+++ remove_tmp_files.bat
@@ -7,11 +7,12 @@ del iprofile.a
 del iprofile.b
 del ifileIO3.a
 del ifileIO3_.s
-del w2fileIO3.a
-del w2fileIO3.s
+del wfileIO3.a
+del wfileIO3.s
 del _startup1.s
 del _startup2.s
 del _startup1.go
 del itrace.a
 del itrace.b
-
+del wcon_imports.a
+del wcon_imports.cpp
