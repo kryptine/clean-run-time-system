fef95903cada1a17414ab85e6f06dd4b863fa039
John van Groningen <johnvg@cs.ru.nl>
2006-07-18 11:31:38 +0000

    add batch file for building object files for 64 bit windows

---
+++ build_windows_object_files_64.bat
@@ -0,0 +1,7 @@
+call "c:\Program Files\Microsoft SDK\SetEnv.bat" /AMD64 /RETAIL
+ml64 /nologo /c /Fo _startup1.o /c astartup.asm
+ml64 /nologo /c /DPROFILE /Fo _startup1Profile.o astartup.asm
+ml64 /nologo /c /DPROFILE /DTRACE /Fo _startup1Trace.o astartup.asm
+cl /nologo /c /O /DWINDOWS /DTIME_PROFILE /DWRITE_HEAP /Fo_startup2.o wcon.c
+"C:\Program Files (x86)\Microsoft Visual Studio 8\VC\bin\x86_amd64\ml64.exe" /nologo /c /Fo _startup3.o afileIO3.asm
+cl /nologo /c /O /DWINDOWS /DA64 /DTIME_PROFILE /Fo_startup4.o wfileIO3.c
