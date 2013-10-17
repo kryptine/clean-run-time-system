37c8e83f9be9a61bcd0f15f82532b41e07a9f4dd
John van Groningen <johnvg@cs.ru.nl>
2013-10-17 12:47:49 +0000

    create static libraries (using libtool), instead of object files (using ld), to work around a bug in ld for MacOSX 10.7 and 10.8, that forgets to export some symbols defined in areals.s

--- macho64/make.sh
+++ macho64/make.sh
@@ -3,5 +3,9 @@ as startupTrace.s -o startupTrace.o
 (cd ..; cc -c -O -DI486 -DMACH_O64 scon.c)
 (cd ..; cc -c -O -DUSE_CLIB -DLINUX -DI486 -DGNU_C -DMACH_O64 -o macho64/ufileIO2.o ./ufileIO2.c)
 as afileIO3.s -o afileIO3.o
-ld -r startup.o ../scon.o afileIO3.o ufileIO2.o -o _startup.o
-ld -r startupTrace.o ../scon.o afileIO3.o ufileIO2.o -o _startupTrace.o
+# Because of a bug in ld, the following does not work anymore (MacOSX 10.7 and 10.8)
+# ld -r startup.o ../scon.o afileIO3.o ufileIO2.o -o _startup.o
+libtool -static -o _startup.o startup.o ../scon.o afileIO3.o ufileIO2.o
+# Because of a bug in ld, the following does not work anymore (MacOSX 10.7 and 10.8)
+# ld -r startupTrace.o ../scon.o afileIO3.o ufileIO2.o -o _startupTrace.o
+libtool -static -o _startupTrace.o ../scon.o afileIO3.o ufileIO2.o
