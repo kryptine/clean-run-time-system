c370bac698f548aca4d8648dd92f9df3b4c06214
John van Groningen <johnvg@cs.ru.nl>
2013-10-18 13:40:13 +0000

    use gcc instead of cc (llvm)

--- macho64/make.sh
+++ macho64/make.sh
@@ -1,7 +1,7 @@
 as startup.s -o startup.o
 as startupTrace.s -o startupTrace.o
-(cd ..; cc -c -O -DI486 -DMACH_O64 scon.c)
-(cd ..; cc -c -O -DUSE_CLIB -DLINUX -DI486 -DGNU_C -DMACH_O64 -o macho64/ufileIO2.o ./ufileIO2.c)
+(cd ..; gcc -c -O -DI486 -DMACH_O64 scon.c)
+(cd ..; gcc -c -O -DUSE_CLIB -DLINUX -DI486 -DGNU_C -DMACH_O64 -o macho64/ufileIO2.o ./ufileIO2.c)
 as afileIO3.s -o afileIO3.o
 # Because of a bug in ld, the following does not work anymore (MacOSX 10.7 and 10.8)
 # ld -r startup.o ../scon.o afileIO3.o ufileIO2.o -o _startup.o
