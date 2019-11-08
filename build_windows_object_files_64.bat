40fc19d0914ceffffb117a5a50caeb1949e9b4a2
johnvg@science.ru.nl <johnvg@science.ru.nl>
2019-11-08 12:43:29 +0100

    use INT label instead of dINT on amd64 linux

--- build_windows_object_files_64.bat
+++ build_windows_object_files_64.bat
@@ -6,7 +6,7 @@ ml64 /nologo /c /Fo _startup1_.o astartup.asm
 ml64 /nologo /c /DPROFILE /Fo _startup1Profile_.o astartup.asm
 .\rename_Tn_sections_and_dINT _startup1Profile_.o _startup1Profile.o
 ml64 /nologo /c /DPROFILE /DTRACE /Fo _startup1Trace_.o astartup.asm
-.\rename_Tn_sections_and_dINt _startup1Trace_.o _startup1Trace.o
+.\rename_Tn_sections_and_dINT _startup1Trace_.o _startup1Trace.o
 cl /nologo /c /O /DWINDOWS /DTIME_PROFILE /DWRITE_HEAP /Fo_startup2.o wcon.c
 "C:\Program Files (x86)\Microsoft Visual Studio 8\VC\bin\x86_amd64\ml64.exe" /nologo /c /Fo _startup3.o afileIO3.asm
 cl /nologo /c /O /DWINDOWS /DA64 /DTIME_PROFILE /Fo_startup4.o wfileIO3.c
