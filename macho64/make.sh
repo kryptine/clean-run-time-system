f4ba953fe6fe8543c29b759b0a51b862669026bc
Camil Staps <info@camilstaps.nl>
2019-12-16 09:53:45 +0100

    Fix make.sh for macho64 (custom scon.o for callgraph profiling)

--- macho64/make.sh
+++ macho64/make.sh
@@ -4,6 +4,7 @@ as startupProfile.s -o startupProfile.o
 as startupProfileGraph.s -o startupProfileGraph.o
 (cd ..; gcc -c -O -DI486 -DMACH_O64 scon.c -o scon.o)
 (cd ..; gcc -c -O -DI486 -DMACH_O64 -DTIME_PROFILE -DPROFILE scon.c -o scon-profile.o)
+(cd ..; gcc -c -O -DI486 -DMACH_O64 -DTIME_PROFILE -DPROFILE -DPROFILE_GRAPH scon.c -o scon-profile-graph.o)
 (cd ..; gcc -c -O -DI486 -DMACH_O64 -DTIME_PROFILE -DPROFILE -DTRACE scon.c -o scon-trace.o)
 (cd ..; gcc -c -O -DUSE_CLIB -DLINUX -DI486 -DGNU_C -DMACH_O64 -o macho64/ufileIO2.o ./ufileIO2.c)
 (cd ..; gcc -c -g -DUSE_CLIB -DLINUX -DI486 -DGNU_C -DMACH_O64 -o macho64/profile_graph.o ./profile_graph.c)
@@ -17,4 +18,4 @@ libtool -static -o _startupTrace.o startupTrace.o ../scon-trace.o afileIO3.o ufi
 # Because of a bug in ld, the following does not work anymore (MacOSX 10.7 and 10.8)
 # ld -r startupProfile.o ../scon-profile.o afileIO3.o ufileIO2.o -o _startupProfile.o
 libtool -static -o _startupProfile.o startupProfile.o ../scon-profile.o afileIO3.o ufileIO2.o
-libtool -static -o _startupProfileGraph.o startupProfileGraph.o ../scon-profile.o afileIO3.o ufileIO2.o profile_graph.o
+libtool -static -o _startupProfileGraph.o startupProfileGraph.o ../scon-profile-graph.o afileIO3.o ufileIO2.o profile_graph.o
